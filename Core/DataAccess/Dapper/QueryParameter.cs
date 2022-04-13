f88ff7159e201ffe320362566d0701b12619fc55
yazilimalkapida@gmail.com <yazilimalkapida@gmail.com>
2022-04-13 17:08:21 +0300

    save

--- Core/DataAccess/Dapper/QueryParameter.cs
+++ Core/DataAccess/Dapper/QueryParameter.cs
@@ -12,7 +12,7 @@ namespace Core.DataAccess.Dapper
          /// GetAll ? true >>>>> select * from table 
          /// : false >>>>> select * from table where... order by ... offset  @RowNumber rows fetch next @RowPerPage rows
          /// </summary>
-        public bool GetAll { get; set; } = true;
+        public bool GetAll { get; set; } = false;
         public int RowNumber { get; set; } = 0;
         public int RowPerPage { get; set; } = 30;
         public int TopRow { get; set; } = 0;
